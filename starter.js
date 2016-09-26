54eaec43c7b44c5e3e95a6d4dc6122eec285efb5
Zeb Tim Girouard <ZebGirouard@users.noreply.github.com>
2016-09-26 09:26:06 -0600

    Create starter.js

---
+++ starter.js
@@ -0,0 +1,29 @@
+// Functions and non-DOM variables defined up here
+
+var getData = function(movieTitle){
+
+
+}
+
+var addAJAXFunction = function(){
+
+
+}
+
+var skeletonizeMovie = function(name,imagePath){
+
+
+}
+
+var handleResponse = function(data){
+
+  
+}
+
+
+// Actual invocation & DOM manipulation within the $(document).ready(function(){}) block
+$(document).ready(function(){
+
+
+
+})
